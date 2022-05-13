93aaa0e3a52030f311b7d84980fb9b2ebe15dd4e
Lulzpower <Zaddicus1@gmail.com>
2022-05-13 08:43:19 -0600

    completed setup for back end including get and post views

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    // api/games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            var VideoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+
+            return Ok(VideoGamePublishers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesById(int id)
+        {
+            var VideoGames = _context.VideoGames.Where(vg => vg.Id == id);
+            return Ok(VideoGames);
+        }
+    }
+}
